229e1ef6babbf508ef7621c84f1f0ae5bc5fe061
shubhamkumawat-kc <shubham@karmacircles.com>
2021-12-28 16:03:10 +0530

    first

---
+++ src/Components/Charts/Charts.tsx
@@ -0,0 +1,16 @@
+import './Charts.css';
+import data from './../../Data/data'
+function  Charts() {
+    const info =data;
+    console.log(info);
+    
+  return (
+    <div className="">
+        <header className="">
+            charts    
+        </header>
+    </div>
+  );
+}
+
+export default Charts;
